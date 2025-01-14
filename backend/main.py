bf44fe4ec5efcbff969db8fa3c9b5413d6010327
aladdin1011 <nurmakov2015@gmail.com>
2025-01-14 21:28:15 +0500

    Add db and dotenv

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os 
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+response = supabase.table("country").select("*").execute()
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def read_root():
